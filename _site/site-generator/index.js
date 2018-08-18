f24287f41870ff6aeef398918e5a537d304196a8
Jack <hwguyguy@gmail.com>
2018-08-18 18:56:20 +0800

    Add sitemap

--- _site/site-generator/index.js
+++ _site/site-generator/index.js
@@ -46,6 +46,20 @@ class SiteGenerator {
 			mkdirp.sync(_dir)
 			writeFileSync(filePath, content)
 		}
+
+		this.writeSitemap(dir + '/sitemap.xml')
+	}
+
+	writeSitemap(filePath) {
+		let content = '<?xml version="1.0" encoding="utf-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+
+		for (const path in this.routes) {
+			content += '<url><loc>https://php-rise.github.io' + path + '</loc></url>'
+		}
+
+		content += '</urlset>'
+
+		writeFileSync(filePath, content)
 	}
 }
 
